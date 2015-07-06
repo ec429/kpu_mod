e02672177b8ac2d590a88ba5b5016c71262b2388
Edward Cree <ec429@cantab.net>
2015-07-06 22:38:14 +0100

    Add AssemblyInfo

---
+++ src/KPU/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KPU")]
+[assembly: AssemblyDescription("Kerbal Processing Unit")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KPU")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cbb1a1ab-b004-4084-aa0a-dedfd898fa86")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// Don't include the patch revision in the AssemblyVersion - as this will break any dependent
+// DLLs any time it changes.  Breaking on a minor revision is probably acceptable - it's
+// unlikely that there wouldn't be other breaking changes on a minor version change.
+[assembly: AssemblyVersion("0.0")]
+[assembly: AssemblyFileVersion("0.0.2")]
+
+// Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
+// non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
+// does not appear to have a hard requirement to match the assembly version.
+[assembly: KSPAssembly("KPU", 0, 0)]
