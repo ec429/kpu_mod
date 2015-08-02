db6e7abc33acc07e34b1a26493983c83214b1225
Edward Cree <ec429@cantab.net>
2015-08-02 04:21:50 +0100

    kappa-ray: API version 0.1

--- src/kappa-ray/Properties/AssemblyInfo.cs
+++ src/kappa-ray/Properties/AssemblyInfo.cs
@@ -20,10 +20,10 @@ using System.Runtime.CompilerServices;
 // Don't include the patch revision in the AssemblyVersion - as this will break any dependent
 // DLLs any time it changes.  Breaking on a minor revision is probably acceptable - it's
 // unlikely that there wouldn't be other breaking changes on a minor version change.
-[assembly: AssemblyVersion("0.0")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1.0")]
 
 // Use KSPAssembly to allow other DLLs to make this DLL a dependency in a
 // non-hacky way in KSP.  Format is (AssemblyProduct, major, minor), and it
 // does not appear to have a hard requirement to match the assembly version.
-[assembly: KSPAssembly("kappa-ray", 0, 0)]
+[assembly: KSPAssembly("kappa-ray", 0, 1)]
